64c16574b1dcc7a115dcf201f336d1faaf1350e3
Anthenny <anthenny1997@hotmail.com>
2022-04-13 14:14:02 +0200

    Formulier toegevoegd met de bijhorende dependencies

--- frontend-v2/components/Form.js
+++ frontend-v2/components/Form.js
@@ -1,23 +1,43 @@
 import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native'
 import React, {useState} from 'react'
+import { CheckBox } from 'react-native-elements'
 
 
 export default function Form() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const checkboxHandler = () => {
+    setIsChecked(!isChecked);
+  }
+
+  const submitFormHandler = () => {
+    if (isChecked) console.log('true, je wilt op de hoogte gehouden worden');
+    if(!isChecked) console.log('false, u wilt niet op de hoogte worden gehouden');
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.smallText}>Meld je gratis aan.</Text>
       <Text style={styles.bigText}>Schrijf je nu in voor de <Text style={styles.bigText_span}>TechNight</Text>!</Text>
 
-        <View>
-            <TextInput></TextInput>
-            <TextInput></TextInput>
+        <View style={styles.inputRow}>
+            <TextInput style={styles.inputRow_input} placeholder='Voornaam' />
+            <TextInput style={styles.inputRow_input} placeholder='Achternaam' />
         </View>
 
-        <TextInput placeholder='Email' />
-        <TextInput placeholder='Telefoonnummer'/>
-        <TextInput placeholder='Bedrijfsnaam'/>
-        <TextInput placeholder='Functie'/>
-        <TextInput placeholder='Hoe heeft u van ons gehoord'/>
+          <TextInput style={styles.input} placeholder='Email' />
+          <TextInput style={styles.input} placeholder='Telefoonnummer'/>
+          <TextInput style={styles.input} placeholder='Bedrijfsnaam'/>
+          <TextInput style={styles.input} placeholder='Functie'/>
+          <TextInput style={styles.input} placeholder='Hoe heeft u van ons gehoord'/>
+
+        <CheckBox 
+          center 
+          title="Ik blijf graag op de hoogte van aankomende events"
+          containerStyle ={styles.checkbox}
+          checked={isChecked}
+          checkedColor="#C0345F"
+          onPress={checkboxHandler} />
 
       <TouchableOpacity style={styles.button}>
         <Text style={styles.buttonText}>Ik meld me aan</Text>
@@ -29,7 +49,9 @@ export default function Form() {
 const styles = StyleSheet.create({
     container: {
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        marginLeft: '30%',
+        marginRight: '30%'
     },
     smallText : {
         fontSize: 16,
@@ -37,18 +59,47 @@ const styles = StyleSheet.create({
     },
     bigText: {
         fontSize: 20,
-        marginBottom: 40
+        marginBottom: 20
     },
     bigText_span: {
         color: '#C0345F'
     },
+    inputRow: {
+      flexDirection: 'row',
+      width: '100%',
+      justifyContent: 'space-between'
+    },
+    inputRow_input: {
+      width: '45%',
+      marginBottom: 25,
+      borderWidth: 1,
+      padding: 12,
+      fontSize: 18,
+      borderRadius: 6
+    },
+    input: {
+      width: '100%',
+      marginBottom: 25,
+      borderWidth: 1,
+      padding: 12,
+      fontSize: 18,
+      borderRadius: 6
+    },
+    checkbox: {
+      width: '100%',
+      backgroundColor: 'transparent',
+      borderColor: 'transparent',
+      fontSize: 16,
+      marginBottom: 10
+    },
     button: {
-        width: '40%',
+        width: '100%',
         backgroundColor: '#FF9200',
         borderRadius: 6,
-        padding: 6,
+        padding: 10,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        marginBottom: 80
         
     },
     buttonText : {
