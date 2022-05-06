a8bd811fc5458fdbdcd81806f83733ee856447d6
Anthenny <anthenny1997@hotmail.com>
2022-05-06 09:56:55 +0200

    Begin van frontend-dashboard toegevoegd, DS_Store verwijderd

---
+++ frontend-dashboard/components/sidebar/Sidebar.tsx
@@ -0,0 +1,50 @@
+import type { NextPage } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import avatarImg from '../../public/Dashboard-avatar.png';
+import homeImg from '../../public/Dashboard-home.png';
+import settingsImg from '../../public/Dashboard-settings.png';
+import logoutImg from '../../public/Dashboard-logout.png';
+import styles from '../../styles/Sidebar.module.css';
+
+export const Sidebar: NextPage = () => {
+  const logoutHandler = () => {
+    console.log('logged out');
+  };
+  return (
+    <div className={styles.sidebar}>
+      <div className={styles.sidebar__container}>
+        <div className={styles.sidebar__avatar}>
+          <Image src={avatarImg} alt="avatar" width={117} height={117} />
+        </div>
+        <div className={styles.container}>
+          <div className={styles.sidebar__link_active}>
+            <div className={styles.image_style}>
+              <Image
+                className={styles.sidebar__link_item_img}
+                src={homeImg}
+                alt="icon"
+                width={34}
+                height={34}
+              />
+            </div>
+            <p>Home</p>
+          </div>
+          <div className={styles.sidebar__link}>
+            <div className={styles.image_style}>
+              <Image src={settingsImg} alt="icon" width={34} height={34} />
+            </div>
+            <p>Settings</p>
+          </div>
+          <div onClick={logoutHandler} className={styles.sidebar__link}>
+            <div className={styles.image_style}>
+              <Image src={logoutImg} alt="icon" width={34} height={34} />
+            </div>
+            <p>Logout</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
