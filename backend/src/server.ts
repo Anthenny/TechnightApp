f5fe31b92ad6c96d61f3aed33bf7c2cd17a7c727
Anthenny <a.dehoon@developers.nl>
2022-06-08 12:54:57 +0200

    We pushen nu de config dir naar github, en heb de .DS_Store verwijderd

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,11 +9,12 @@ const port = process.env.PORT;
 // Lokale database die we gebruiken met ontwikkelen
 const mongoDevURL = `mongodb://${process.env.MONGO_USER_DEV}:${process.env.MONGO_PASSWORD_DEV}@${process.env.MONGO_IP}:${process.env.MONGO_PORT}/${process.env.DB_NAME}?authSource=admin`;
 
+// TODO Met ivo overleggen via waar we de productie database gaan hosten een developers.nl acc?
 // Productie database
 const mongoProdURL = `mongodb+srv://productieAdminAnnasAccessoires:DitIsHetSuperGeheimeWachtWoordQwErTyUIOP@cluster0.cpirwvr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 mongoose
-  .connect(mongoDevURL)
+  .connect(mongoProdURL)
   .then(async () => {
     await createAdminIfNotExists();
     app.listen(port, () => {
